f7e7f99045379eeffb0f240a03a952259c827f46
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-01-26 22:37:06 +0100

    backend init

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const app = express()
+
+// routes
+
+app.get('/', (req, res) => {
+    try {
+        res.json('Get Request')
+    } catch (error) {
+     res.json(error)   
+    }
+})
+
+
+app.listen(8080, () => {
+    console.log('server connected to http://localhost:8080')
+})
