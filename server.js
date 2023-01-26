8c7848d24fca8ff0ddc9c43c5f1ee25af485eb2f
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-01-26 22:59:26 +0100

    endpoints created

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import { config } from 'dotenv'
+
 
 const app = express()
+// app middleware
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json())
+config()
+
+
+// app port
+
+const port = process.env.PORT || 8080
+
 
 // routes
+
+
 
 app.get('/', (req, res) => {
     try {
@@ -13,6 +30,6 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(8080, () => {
-    console.log('server connected to http://localhost:8080')
+app.listen(port, () => {
+    console.log(`'server connected to http://localhost:${port}'`)
 })
