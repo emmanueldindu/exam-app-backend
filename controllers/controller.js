e28bc992d95ae7d573fc50aec16837cc9ca5ffd5
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-01-28 01:29:06 +0100

    API

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,6 @@
 import Questions from "../models/questionSchema.js"
 import Results from "../models/resultSchema.js"
+import questions, {answers} from '../database/data.js'
 
 
 export async function getQuestions(req, res) {
@@ -14,24 +15,53 @@ export async function getQuestions(req, res) {
 
 export async function insertQuestions(req, res) {
    try {
-    Questions.insertMany({questions: [0], answers:[1]})
+       Questions.insertMany({  questions,  answers }, function (err, data) {
+        res.json({msg:"Data saved successfully...!"})
+    })
    } catch (error) {
     res.json({error})
    }
 }
 
 export async function deleteQuestions(req, res) {
-    res.json('delete question api')
+   try {
+       await Questions.deleteMany();
+       res.json({msg: "Questions deleted Successfullt....!"})
+   } catch (error) {
+    res.json({error})
+   }
 }
 
+
+
 export async function getResults(req, res) {
-    res.json('get results')
+   try {
+       const r = await Results.find()
+       res.json(r)
+       
+   } catch (error) {
+    res.json({error})
+   }
 }
 
 export async function postResults(req, res) {
-    res.json('post results')
+   try {
+    const {username, result, attempts, points, achived} =   req.body;
+       if (!username && !result) throw new Error('Data Not Provided....')
+       
+       Results.create({ username, result, attempts, points, achived }, function (err, data) {
+           res.json({msg: "Result Saved Successfully..."})
+       } )
+   } catch (error) {
+    res.json({error})
+   }
 }
 
 export async function deleteResults(req, res) {
-    res.json('delete results')
+  try {
+      await Results.deleteMany()
+      res.json({msg: "Result Deleted Successfully"})
+  } catch (error) {
+    res.json({error})
+  }
 }
---
+++ database/data.js
@@ -0,0 +1,144 @@
+export default [
+    {
+        id: 1,
+        question : "The moons, Miranda, Ariel, Umbriel, Titania and Oberon orbit which planet?",
+        options: [
+            'Jupiter',
+            'venus',
+            'Uranus',
+            'Neptune'
+        ]
+
+    },
+
+
+    {
+        id: 2,
+        question : "Which of these stars is the largest",
+        options: [
+            'VY Canis Majoris',
+            'UY Scuti',
+            'Betelgeuse',
+            'RW Cephei'
+        ]
+
+    },
+
+    {
+        id: 3,
+        question : "What mineral has the lowest number on the Mohs scale",
+        options: [
+            'Talc',
+            'Quartz',
+            'Diamond',
+            'Gypsum'
+        ]
+
+    },
+
+    {
+        id: 4,
+        question : "All the following metal elements are liquids at or near room temperature EXCEPT",
+        options: [
+            'Mecury',
+            'Gallium',
+            'Caseium',
+            'Beryllium'
+        ]
+
+
+    },
+    {
+        id: 5,
+        question : "Who developed the first successful polio vaccine in the 1950s?",
+        options: [
+            'John F. Enders', 
+            'Thomas Weller',
+            'Jonas Salk',
+            'Frederick Robbins',
+            
+        ]
+
+
+    },
+
+
+    {
+        id: 6,
+        question : "Myopia is the scientific term for which condition?",
+        options: [
+            'Farsightedness',
+            'Double Vision',
+            'Clouded Vision',
+            'Shortsightedness',
+            
+        ]
+
+
+    },
+
+    {
+        id: 7,
+        question : "Which of these elements on the Periodic Table is a Noble Gas",
+        options: [
+            'Neon', 
+            'Potassium',
+            'Iodine',
+            'Colbalt'
+            
+        ]
+
+
+    },
+
+    {
+        id: 8,
+        question : "Who made the discovery of X-rays?",
+        options: [
+            'Thomas Alva Edison',
+            'James Watt', 
+
+            "Wilhelm Conrad R&ouml;ntgen",
+            'Albert Einstein',            
+        ]
+
+
+    },
+
+    {
+        id: 9,
+        question : "About how old is Earth?",
+        options: [
+            '3.5 Billion Years', 
+            '4.5 Billion Years',
+            '2.5 Billion Years', '5.5 Billion Years',
+        ]
+
+
+    },
+
+    {
+        id: 10,
+        question : "What are human nails made of?",
+        options: [
+            'Bone',
+            'Keratin',
+             'Chitin',
+            'Calcium'
+        ]
+
+
+    },
+
+
+
+
+
+
+
+
+]
+
+// 2 0 0 3 2 3 0 2 1 1
+
+export const answers = [2, 0, 0, 3, 2, 3, 0, 2, 1, 1]
